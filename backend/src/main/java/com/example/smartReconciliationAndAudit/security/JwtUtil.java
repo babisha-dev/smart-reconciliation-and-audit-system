97208ad7032ec5876064a9220aeb841485aa3a7d
babisha04 <babishariya@gmail.com>
2026-04-14 22:20:35 +0530

    feat: implement jwtUtil with token generation and username extraction

--- backend/src/main/java/com/example/smartReconciliationAndAudit/security/JwtUtil.java
+++ backend/src/main/java/com/example/smartReconciliationAndAudit/security/JwtUtil.java
@@ -1,13 +1,45 @@
 package com.example.smartReconciliationAndAudit.security;
 
-import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
+
+
 
 @Component
 public class JwtUtil {
-    private  String secret;
+    @Value("${jwt.secret}") private String secret;
+    @Value("${jwt.expiration}") private Long expiration;
     public Key key(){
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
+    public String generate(UserDetails u, String role)
+    {
+        return Jwts.builder()
+                .setClaims(Map.of("role",role))
+                .setSubject(u.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+  public String username(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+  }
+
+
 }
