dd6b5aaf7569887bd8d30788995af2e019b8f356
babisha04 <babishariya@gmail.com>
2026-04-16 11:28:57 +0530

    feat: configure BCryptPasswordEncoder and AuthenticationManager for authentication

--- backend/src/main/java/com/example/smartReconciliationAndAudit/config/SecurityConfig.java
+++ backend/src/main/java/com/example/smartReconciliationAndAudit/config/SecurityConfig.java
@@ -6,11 +6,16 @@ import com.example.smartReconciliationAndAudit.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.file.ConfigurationSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationConverter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
@@ -52,5 +57,13 @@ return http.build();
               src.registerCorsConfiguration("/**",config);
               return src;
 
+   }
+   @Bean
+    public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+   }
+   @Bean
+   public AuthenticationManager authManager(AuthenticationConfiguration cfg) throws Exception{
+return cfg.getAuthenticationManager();
    }
 }
