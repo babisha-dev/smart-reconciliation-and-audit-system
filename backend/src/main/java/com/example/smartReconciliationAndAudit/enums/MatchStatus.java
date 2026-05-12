5f96e62518b73a268a03c2cf929a7fa913e0e74e
babisha04 <babishariya@gmail.com>
2026-05-12 23:56:39 +0530

    feat: implemented custom methods for Transaction Repo

--- backend/src/main/java/com/example/smartReconciliationAndAudit/enums/MatchStatus.java
+++ backend/src/main/java/com/example/smartReconciliationAndAudit/enums/MatchStatus.java
@@ -1,4 +1,4 @@
 package com.example.smartReconciliationAndAudit.enums;
 
-public enum MatchStatus { COMPLETED, PARTIALLY_COMPLETED, MISMATCHED,DUPLICATE
+public enum MatchStatus { MATCHED, PARTIALLY_MATCHED, MISMATCHED,DUPLICATE
 }
--- backend/src/main/java/com/example/smartReconciliationAndAudit/repository/TransactionRecordRepository.java
+++ backend/src/main/java/com/example/smartReconciliationAndAudit/repository/TransactionRecordRepository.java
@@ -2,6 +2,18 @@ package com.example.smartReconciliationAndAudit.repository;
 
 import com.example.smartReconciliationAndAudit.model.TransactionRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TransactionRecordRepository extends JpaRepository<TransactionRecord,Long> {
+    List<TransactionRecord> findByUploadJobId(Long uploadJobId);
+
+    @Query("SELECT r FROM TransactionRecord r WHERE r.isSystemRecord = true AND r.transactionId = ?1")
+    Optional<TransactionRecord> findSystemByUploadJobId(Long uploadJobId);
+
+    @Query("SELECT r FROM TransactionRecord r WHERE r.isSystemRecord = true AND r.referenceNumber = ?1")
+    Optional<TransactionRecord> findSystemByReferenceNumber(String referenceNumber);
+
 }
