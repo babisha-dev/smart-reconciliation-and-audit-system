d76e59284255fb0705461bb2c365eff175493d38
babisha04 <babishariya@gmail.com>
2026-05-28 23:20:44 +0530

    feat: implemented hash method

---
+++ backend/src/main/java/com/example/smartReconciliationAndAudit/service/FileProcessingService.java
@@ -0,0 +1,22 @@
+package com.example.smartReconciliationAndAudit.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.security.MessageDigest;
+
+@Service
+@RequiredArgsConstructor
+public class FileProcessingService {
+
+    public String hash(MultipartFile file) throws Exception{
+        byte[] hash= MessageDigest.getInstance("SHA-256").digest(file.getBytes());
+StringBuilder sb=new StringBuilder();
+for(Byte b:hash) {
+    sb.append(String.format("%02x", b));
+}
+return sb.toString();
+
+    }
+}
