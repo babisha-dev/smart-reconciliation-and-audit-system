d03448d6b65500551a43e80b3b6bee9b40f7fc13
babisha04 <babishariya@gmail.com>
2026-07-06 14:24:06 +0530

    feat: Implement file preview endpoint

--- backend/src/main/java/com/example/smartReconciliationAndAudit/controller/UploadController.java
+++ backend/src/main/java/com/example/smartReconciliationAndAudit/controller/UploadController.java
@@ -1,11 +1,20 @@
 package com.example.smartReconciliationAndAudit.controller;
 
+import com.example.smartReconciliationAndAudit.dto.ApiResponse;
+import com.example.smartReconciliationAndAudit.service.FileProcessingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/upload")
@@ -13,4 +22,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name="2-File upload")
 @SecurityRequirement(name="bearerAuth")
 public class UploadController {
+    private  final  FileProcessingService fps;
+
+    @PostMapping("/preview")
+    public ResponseEntity<ApiResponse<List<Map<String,String>>>> preview(@RequestParam("file") MultipartFile file){
+        try{
+            return ResponseEntity.ok(ApiResponse.ok(fps.preview(file)));
+        }
+        catch(Exception e){
+             return ResponseEntity.badRequest().body(ApiResponse.error("Preview failed" +e.getMessage()));
+        }
+    }
 }
